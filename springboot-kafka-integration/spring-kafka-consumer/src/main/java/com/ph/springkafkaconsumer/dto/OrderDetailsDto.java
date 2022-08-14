3e60fb6700ec3da61a43094fbe7177728423c6b0
pratip1990 <pratip.halder@gmail.com>
2022-08-14 18:53:37 +0530

    Committed for Kafka consumer

--- springboot-kafka-integration/spring-kafka-consumer/src/main/java/com/ph/springkafkaconsumer/dto/OrderDetailsDto.java
+++ springboot-kafka-integration/spring-kafka-consumer/src/main/java/com/ph/springkafkaconsumer/dto/OrderDetailsDto.java
@@ -4,10 +4,34 @@ import lombok.Data;
 
 import java.io.Serializable;
 
-@Data
-public class OrderDetailsDto {
+public class OrderDetailsDto implements Serializable{
+    private static final long serialVersionUID = 1l;
     private Long orderId;
     private Long customerId;
-    private Long restaurantId;
+    private Long providerId;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getProviderId() {
+        return providerId;
+    }
+
+    public void setProviderId(Long providerId) {
+        this.providerId = providerId;
+    }
 }
 
--- springboot-kafka-integration/spring-kafka-consumer/src/main/java/com/ph/springkafkaconsumer/listener/KafkaConsumer.java
+++ springboot-kafka-integration/spring-kafka-consumer/src/main/java/com/ph/springkafkaconsumer/listener/KafkaConsumer.java
@@ -1,12 +1,21 @@
 package com.ph.springkafkaconsumer.listener;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ph.springkafkaconsumer.dto.OrderDetailsDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaConsumer {
+    @Autowired
+    ObjectMapper mapper;
+
     @KafkaListener(topics = "order_topic",groupId = "group_id")
-    public void consumesOrder(String message){
-        System.out.println("message = " + message);
+    public void consumesOrder(String message) throws JsonProcessingException {
+        System.out.println("Message : "+message);
+        OrderDetailsDto orderDetails = mapper.readValue(message, OrderDetailsDto.class);
     }
 }
